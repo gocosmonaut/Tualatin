78c2c883ca2bc52629778fd2105bb275a58ea062
Daniel Deitschel <daniel@gocosmonaut.com>
2013-10-19 16:38:00 -0700

    Basic theming

--- trunk/web/sites/all/themes/tualatin/templates/page.tpl.php
+++ trunk/web/sites/all/themes/tualatin/templates/page.tpl.php
@@ -185,7 +185,7 @@
         </div><!-- /#featured -->
         <?php endif; ?>
         
-	</div>
+	
         
 	<div id="footer" >
         <div class="container">
@@ -209,13 +209,7 @@
                 
                 <div class="clear"></div>
                 
-                <div id="credits">
-                <?php print(date('Y') . ' ');?>
-                <?php if (!empty($site_name)):?>
-                <?php print $site_name;?>- This is a Free Drupal Theme<br/>
-                <?php endif;?>
-                Ported to Drupal for the Open Source Community by <a href="http://www.drupalizing.com" target="_blank">Drupalizing</a>, a Project of <a href="http://www.morethanthemes.com" target="_blank">More than (just) Themes</a>. Original design by <a href="http://www.simplethemes.com/" target="_blank">Simple Themes</a>.
-                </div>
+                </div> <!-- div tag moved down to put footer inside container  -->
         	</div>
         </div>
     </div>
