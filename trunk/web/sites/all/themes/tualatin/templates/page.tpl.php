dc4635efabd69a7cca82601b912614f8530f5974
Daniel Deitschel <daniel@gocosmonaut.com>
2013-10-21 23:21:29 -0700

    Search

--- trunk/web/sites/all/themes/tualatin/templates/page.tpl.php
+++ trunk/web/sites/all/themes/tualatin/templates/page.tpl.php
@@ -4,6 +4,12 @@
         <?php if ($page['header_top_left'] || $page['header_top_right']): ?>
         <!-- #header-top -->
         <div id="header-top" class="sixteen columns clearfix">
+        
+        <?php if ($logo): ?>
+                  <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo">
+                    <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
+                  </a>
+                <?php endif; ?>
             
             <?php if ($page['header_top_left'] && $page['header_top_right']) { ?>
             <div class="one_half">
@@ -34,11 +40,7 @@
         
             <div class="inner">
     
-                <?php if ($logo): ?>
-                  <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo">
-                    <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
-                  </a>
-                <?php endif; ?>
+                
                 
                 <?php if ($site_name || $site_slogan): ?>
                 <div id="name-and-slogan"<?php if ($hide_site_name && $hide_site_slogan) { print ' class="element-invisible"'; } ?>>
